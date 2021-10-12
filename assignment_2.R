e78694179ed4bacd6a6bf7295f797fcd2b9c6b9d
Micah Villarreal <micahjvillarreal@gmail.com>
2021-10-11 17:58:22 -0700

    Question 2d

--- assignment_2.R
+++ assignment_2.R
@@ -16,3 +16,6 @@ neighborhoods <- neighborhoods %>% head(n, n=20)
 
 #2c
 airbnb_top_neighborhoods <- airbnb %>% filter(neighborhood %in% neighborhoods$neighborhood)
+
+#2d
+summary_stats_top_neighborhoods <- summarize(airbnb_top_neighborhoods %>% group_by(neighborhood),avg_square_feet = mean(square_feet,na.rm=TRUE),avg_price = mean(price,na.rm=TRUE),sd_price=sd(price,na.rm=TRUE), max_price=max(price,na.rm=TRUE), min_price=min(price,na.rm=TRUE))
